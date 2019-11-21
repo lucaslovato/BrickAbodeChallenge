be1e5237ec05517bf3e2ae3b57edb93f87f1352f
Lucas Lovato <lucaslovato@brickabode.com>
2019-11-21 18:41:00 -0300

    Updates

---
+++ precache-manifest.70006346439632bf3bd468d778257fd3.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f909cd4272ca6610a2c47e2e692a0d49",
+    "url": "/BrickAbodeChallenge/index.html"
+  },
+  {
+    "revision": "65d63788c57689861f72",
+    "url": "/BrickAbodeChallenge/static/js/2.b9df36c3.chunk.js"
+  },
+  {
+    "revision": "279480c80018d9c7607f",
+    "url": "/BrickAbodeChallenge/static/js/main.db1ca03f.chunk.js"
+  },
+  {
+    "revision": "b2381c70f9f5e0e78870",
+    "url": "/BrickAbodeChallenge/static/js/runtime-main.154a09a7.js"
+  },
+  {
+    "revision": "9c3789887ffb6858c05b6fbb05677f88",
+    "url": "/BrickAbodeChallenge/static/media/ATL.9c378988.png"
+  },
+  {
+    "revision": "dfe8d42a5600a4168edb41c350f706f4",
+    "url": "/BrickAbodeChallenge/static/media/BKN.dfe8d42a.png"
+  },
+  {
+    "revision": "9e70f40571b5d6da3082b366d2e50305",
+    "url": "/BrickAbodeChallenge/static/media/BOS.9e70f405.png"
+  },
+  {
+    "revision": "39ec47c9f929ff44fd9f3e89be8094fb",
+    "url": "/BrickAbodeChallenge/static/media/CHA.39ec47c9.png"
+  },
+  {
+    "revision": "9bc42b1a606957a34a57111b8b8bcc22",
+    "url": "/BrickAbodeChallenge/static/media/CHI.9bc42b1a.png"
+  },
+  {
+    "revision": "8bafa2b1fb634aaf2ed73a918ed727e2",
+    "url": "/BrickAbodeChallenge/static/media/CLE.8bafa2b1.png"
+  },
+  {
+    "revision": "4748fcc0e628b569f9e577dd5a20f716",
+    "url": "/BrickAbodeChallenge/static/media/DAL.4748fcc0.png"
+  },
+  {
+    "revision": "6d755b70e5914dedf5f65a27c2a7d62d",
+    "url": "/BrickAbodeChallenge/static/media/DEN.6d755b70.png"
+  },
+  {
+    "revision": "bb747ebe740cd167df5afa139438385e",
+    "url": "/BrickAbodeChallenge/static/media/DET.bb747ebe.png"
+  },
+  {
+    "revision": "033c03fe2aee0c5a8e322410a8ed349a",
+    "url": "/BrickAbodeChallenge/static/media/GSW.033c03fe.png"
+  },
+  {
+    "revision": "244ab01431f9d58ffc7d8b42824a5f2e",
+    "url": "/BrickAbodeChallenge/static/media/HOU.244ab014.png"
+  },
+  {
+    "revision": "515e69c3164a8dec493b781b7819d021",
+    "url": "/BrickAbodeChallenge/static/media/IND.515e69c3.png"
+  },
+  {
+    "revision": "45bdc7c4b34c644f1f08770a496e7361",
+    "url": "/BrickAbodeChallenge/static/media/LAC.45bdc7c4.png"
+  },
+  {
+    "revision": "ee83c1338cceba100571419c01e24a08",
+    "url": "/BrickAbodeChallenge/static/media/LAL.ee83c133.png"
+  },
+  {
+    "revision": "a69f108d0637af5b3ce2d273d8d37283",
+    "url": "/BrickAbodeChallenge/static/media/MEM.a69f108d.png"
+  },
+  {
+    "revision": "2fb21bc1e04ce0033016de3dd2c6fbfb",
+    "url": "/BrickAbodeChallenge/static/media/MIA.2fb21bc1.png"
+  },
+  {
+    "revision": "e3573ed0ad3eff7a45025cb67c4c5ef9",
+    "url": "/BrickAbodeChallenge/static/media/MIL.e3573ed0.png"
+  },
+  {
+    "revision": "1284c3eb9c099dd9482f3763854c8ec1",
+    "url": "/BrickAbodeChallenge/static/media/MIN.1284c3eb.png"
+  },
+  {
+    "revision": "cba659c84435df46de1eb733862b6249",
+    "url": "/BrickAbodeChallenge/static/media/NOP.cba659c8.png"
+  },
+  {
+    "revision": "af06fdeecc13bd49faea8b1aba6d5056",
+    "url": "/BrickAbodeChallenge/static/media/NYK.af06fdee.png"
+  },
+  {
+    "revision": "ffdec0ba4de24fa3572954529d2e34cb",
+    "url": "/BrickAbodeChallenge/static/media/OKC.ffdec0ba.png"
+  },
+  {
+    "revision": "31dfd12ad9a23feaca8cd13b32bc81fa",
+    "url": "/BrickAbodeChallenge/static/media/ORL.31dfd12a.png"
+  },
+  {
+    "revision": "6813ee8984342a519388eaaf32520ee6",
+    "url": "/BrickAbodeChallenge/static/media/PHI.6813ee89.png"
+  },
+  {
+    "revision": "15bfcab768aaba638230b20a4b9dcfa9",
+    "url": "/BrickAbodeChallenge/static/media/PHX.15bfcab7.png"
+  },
+  {
+    "revision": "a4be07bd4fbc9f886620bf0cff125fdf",
+    "url": "/BrickAbodeChallenge/static/media/POR.a4be07bd.png"
+  },
+  {
+    "revision": "a9cd1ea9ef44a9aada5274b6744cc0f7",
+    "url": "/BrickAbodeChallenge/static/media/SAC.a9cd1ea9.png"
+  },
+  {
+    "revision": "5f3d427d55e936af141d5341020031b4",
+    "url": "/BrickAbodeChallenge/static/media/SAS.5f3d427d.png"
+  },
+  {
+    "revision": "69729cea341cac659fd99cefb3d2a751",
+    "url": "/BrickAbodeChallenge/static/media/TOR.69729cea.png"
+  },
+  {
+    "revision": "a52ccf1f0d679130ee7670a695c9b95e",
+    "url": "/BrickAbodeChallenge/static/media/UTA.a52ccf1f.png"
+  },
+  {
+    "revision": "f633469d05e508eab0e5a017d210fef0",
+    "url": "/BrickAbodeChallenge/static/media/WAS.f633469d.png"
+  }
+]);
